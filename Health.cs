b3eabcf52946c0e99e5761b6e7a3a2d4da05f198
Oak <kristapsimportant@gmail.com>
2026-04-15 02:54:12 -0700

    Enhance Health class with UI slider support
    Added health slider UI update functionality, made it update

--- Health.cs
+++ Health.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class Health : MonoBehaviour
@@ -6,21 +7,34 @@ public class Health : MonoBehaviour
     public float maxHealth = 100f;
     public float currentHealth;
     public UnityEvent onDeath; // Assign in Inspector for death effects
-
+    public Slider healthSlider; // Assign UI Slider in Inspector
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateHealthUI();
     }
 
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
+        UpdateHealthUI();
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Die();
         }
     }
+
+
+
+    void UpdateHealthUI()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+        }
+    }
+
 
     void Die()
     {
