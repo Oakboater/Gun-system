2366bfe3bb8c9f625bed7b214d0f7d10110c8277
Oak <kristapsimportant@gmail.com>
2026-04-20 01:45:33 -0700

    Updated fire rate proper majigityu

--- Gun.cs
+++ Gun.cs
@@ -1,138 +1,124 @@
-    using UnityEngine;
-    using UnityEngine.UI;
-    using TMPro;
-    using System.Collections;
-    public class Gun : MonoBehaviour
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Shooting")]
+    public float damage = 25f;
+    public float range = 100f;
+    public Transform barrelEnd;
+    public Camera playerCamera;
+    public bool canShoot = true;
+    public LayerMask hitLayers;
+
+    [Header("Fire Rate (Per second)")]
+    public float firerate = 10f;  // Set to 10 shots per second (more reasonable for testing)
+    private float nextTimeToFire = 0f;
+
+    [Header("Ammo")]
+    public int maxAmmo = 30;
+    public int currentAmmo;
+    public float reloadTime = 1.5f;
+    private bool isReloading = false;
+
+    [Header("Effects")]
+    public ParticleSystem muzzleFlash; // Optional
+    public GameObject impactEffect;    // Optional hit effect
+
+    [Header("UI")]
+    public TMP_Text ammoText;
+
+    void Start()
     {
-
-        [Header("Shooting")]
-        public float damage = 25f;
-        public float range = 100f;
-        public Transform barrelEnd;
-        public Camera playerCamera;
-        public bool canShoot = true;
-        public LayerMask hitLayers;
-
-        [Header("Fire Rate (Per second)")]
-        public float firerate = 1000f;
-        private float nextTimeToFire = 0f;
-        
-        [Header("Ammo")]
-        public int maxAmmo = 30;
-        public int currentAmmo;
-        public float reloadTime = 1.5f;
-        private bool isReloading = false;
-
-
-        [Header("Effects")]
-        public ParticleSystem muzzleFlash; // Optional
-        public GameObject impactEffect;    // Optional hit effect
-
-        [Header("UI")]
-        public TMP_Text ammoText;
-
-
-        void Start()
-        {
         currentAmmo = maxAmmo;
         UpdateAmmoUI();
-        }
+    }
 
-
-
-        void Update()
+    void Update()
+    {
+        if (canShoot && Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
         {
-            if (canShoot && Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
+            if (currentAmmo > 0)
             {
-                if (currentAmmo > 0)
-                {
-                    Shoot();
-                    currentAmmo--;
-                    UpdateAmmoUI();
-                    nextTimeToFire = Time.time + 1f / firerate; // Set next allowed shot time
-                }
-                else
-                {
-                    StartReloading();
-                }
+                Shoot();
+                currentAmmo--;
+                UpdateAmmoUI();
+                nextTimeToFire = Time.time + 1f / firerate;  // Control rate of fire
             }
-
-            if (Input.GetKeyDown(KeyCode.R))
+            else
             {
                 StartReloading();
             }
         }
 
-         if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             StartReloading();
         }
-       }
-        IEnumerator Delay()
+    }
+
+    void Shoot()
+    {
+        if (muzzleFlash != null)
+            muzzleFlash.Play();
+
+        RaycastHit hit;
+
+        Vector3 origin = playerCamera.transform.position;
+        Vector3 direction = playerCamera.transform.forward;
+
+        if (Physics.Raycast(origin, direction, out hit, range, hitLayers))
         {
-            yield return new WaitForSeconds(1f / firerate);
-            canShoot = true;
-        }
+            Debug.Log("Hit: " + hit.transform.name);
 
-
-            void Shoot()
-        {
-            if (muzzleFlash != null)
-                muzzleFlash.Play();
-
-            RaycastHit hit;
-
-            Vector3 origin = playerCamera.transform.position;
-            Vector3 direction = playerCamera.transform.forward;
-
-            if (Physics.Raycast(origin, direction, out hit, range, hitLayers))
+            Health targetHealth = hit.transform.GetComponent<Health>();
+            if (targetHealth != null)
             {
-                Debug.Log("Hit: " + hit.transform.name);
+                targetHealth.TakeDamage(damage);
+            }
 
-                Health targetHealth = hit.transform.GetComponent<Health>();
-                if (targetHealth != null)
-                {
-                    targetHealth.TakeDamage(damage);
-                }
-
-                if (impactEffect != null)
-                {
-                    GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                    Destroy(impact, 2f);
-                }
+            if (impactEffect != null)
+            {
+                GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impact, 2f);
             }
         }
-        void StartReloading()
-        {
+    }
+
+    void StartReloading()
+    {
         if (!isReloading && currentAmmo < maxAmmo)
         {
             StartCoroutine(Reloading());
         }
     }
-        IEnumerator Reloading()
+
+    IEnumerator Reloading()
+    {
+        isReloading = true;
+        canShoot = false;
+
+        if (ammoText != null)
         {
-            isReloading = true;
-            canShoot = false;
+            ammoText.text = $"Reloading {reloadTime}";
+        }
 
-            if (ammoText != null)
-            {
-                ammoText.text = $"Reloading {reloadTime}";
-            }
+        yield return new WaitForSeconds(reloadTime);
 
-            yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        canShoot = true;
+        isReloading = false;
 
-            currentAmmo = maxAmmo;
-            canShoot = true;
-            isReloading = false;
-
-            UpdateAmmoUI();
+        UpdateAmmoUI();
     }
-        void UpdateAmmoUI()
-        {
+
+    void UpdateAmmoUI()
+    {
         if (ammoText != null)
         {
             ammoText.text = $"{currentAmmo} / {maxAmmo}";
         }
-
-        }
     }
+}
